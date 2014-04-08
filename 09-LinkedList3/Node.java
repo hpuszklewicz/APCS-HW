a6d9a5583a3d389bc98ee1552a965685e01444ac
Hubert <hubertpuszklewicz@gmail.com>
2014-04-08 05:09:40 -0400

    Revert "deleted old folder for something"
    This reverts commit aca7f82f2281319b1277ac99a50d3a23f9953f00.

---
+++ 09-LinkedList3/Node.java
@@ -0,0 +1,28 @@
+public class Node{
+    private String data;
+    private Node next;
+    
+    public Node(String s){
+	data = s;
+    }
+
+    public void setData(String s){
+	data = s;
+    }
+
+    public String getData(){
+	return data;
+    }
+
+    public Node getNext(){
+	return next;
+    }
+
+    public void setNext(Node n){
+	next = n;
+    }
+
+    public String toString(){
+	return "Data: " + data;
+    }
+}
